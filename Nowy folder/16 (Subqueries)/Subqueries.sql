c389de6edbe8773198036f498b67b0bc950e386b
fiszumistrzu <83349634+fiszumistrzu@users.noreply.github.com>
2022-10-11 12:32:12 +0200

    Subquerie LIKE

--- Nowy folder/16 (Subqueries)/Subqueries.sql
+++ Nowy folder/16 (Subqueries)/Subqueries.sql
@@ -1,3 +1,5 @@
+-- wishlists database
+
 SELECT cname, rid
 FROM customers
 JOIN rentals USING(cid);
@@ -9,3 +11,17 @@ FROM rentals;
 SELECT cname,
     (SELECT COUNT(*) rid FROM rentals WHERE customers.cid = rentals.cid) AS "count rid"
 FROM customers;
+
+-- movies database
+
+SELECT DISTINCT pstate
+FROM people
+WHERE pstate LIKE 'O_';
+
+SELECT pfirstname, pstate
+FROM people
+WHERE pstate IN(SELECT pstate FROM people WHERE pstate LIKE 'O_');
+
+SELECT pfirstname, pstate
+FROM people
+WHERE pstate LIKE 'O_';
