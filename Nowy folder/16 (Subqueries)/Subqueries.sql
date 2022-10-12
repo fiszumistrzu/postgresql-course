53ad85b95cb292cc8443215dcb1a121a52a404cc
fiszumistrzu <daniel.rybarczyk@student.wat.edu.pl>
2022-10-12 14:05:06 +0200

    group by

--- Nowy folder/16 (Subqueries)/Subqueries.sql
+++ Nowy folder/16 (Subqueries)/Subqueries.sql
@@ -12,6 +12,23 @@ SELECT cname,
     (SELECT COUNT(*) rid FROM rentals WHERE customers.cid = rentals.cid) AS "count rid"
 FROM customers;
 
+SELECT pcity, COUNT(*)
+FROM people
+GROUP BY pcity;
+
+SELECT DISTINCT pcity,
+    (SELECT COUNT(*)
+    FROM people AS p
+    WHERE p.pcity = people.pcity)
+FROM people;
+
+SELECT pfirstname, COUNT(*) AS "number of items"
+FROM people
+JOIN lists USING(pid)
+JOIN listitems USING(lid)
+GROUP BY pfirstname
+ORDER BY "number of items" DESC;
+
 -- movies database
 
 SELECT DISTINCT pstate
